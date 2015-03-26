8bca9fe40162c934386b09470b60b902c9f03649
ljfranklin <lylejfranklin@gmail.com>
2015-03-25 22:29:59 -0700

    Embedded angular templates in index for top-level pages

--- lylefranklin.com/config.rb
+++ lylefranklin.com/config.rb
@@ -39,8 +39,30 @@ end
 set :layout, false
 page "pages/*", :layout => :page_layout
 
+helpers do
+  def get_templates(dirname='')
+    base_dir = Pathname(File.join(File.dirname(__FILE__), "source"))
+    target_dir = Dir[File.join(base_dir, dirname, "*")]
+    target_dir.select { |filename|
+      File.file?(filename)
+    }.map { |filename|
+      template_name = File.basename(filename, '.*')
+      template_path = Pathname(File.join(File.dirname(filename), template_name))
+      Pathname(template_path).relative_path_from(base_dir).to_s
+    }
+  end
+
+  def partial_with_layout(partial_name, layout_name)
+    wrap_layout layout_name do
+      @page_url = "/" + partial_name
+      partial @page_url
+    end 
+  end
+end
+
 # Build-specific configuration
 configure :build do
+
   activate :minify_css
 
   activate :minify_javascript
