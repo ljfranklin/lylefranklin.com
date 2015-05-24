f5bbecd1b6f7e6034fc5843a58c82291de1e9f6a
ljfranklin <lylejfranklin@gmail.com>
2015-05-24 14:53:39 -0700

    Add test for nav bar link highlighting

--- spec/features/nav_spec.rb
+++ spec/features/nav_spec.rb
@@ -3,17 +3,30 @@ require "spec_helper"
 RSpec.describe "nav", :js => true, :type => :feature do
   include Capybara::Angular::DSL
 
-  before do
-    visit "/"
-  end
-  
   it "contains links to top-level pages" do
+    visit "/"
+
     page_names = get_page_names
     page_names[page_names.index("home")] = ""
 
     expect(page).to have_selector ".navbar"
     page_names.each do |page_name|
       expect(page).to have_selector ".navbar a[href='/#/#{page_name}']"
+    end
+  end
+
+  it "highlights link of current page" do
+    page_names = get_page_names - ["home"]
+
+    page_links = page_names.map do |page_name|
+      "/#/#{page_name}"
+    end
+
+    page_links.each do |link|
+      visit link
+
+      active_link = ".navbar li.active a[href='#{link}']"
+      expect(page).to have_selector active_link
     end
   end
 end
