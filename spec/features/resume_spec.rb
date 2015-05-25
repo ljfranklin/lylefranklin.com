901758cece657d738e73a5d1131d42cbc5372fa4
ljfranklin <lylejfranklin@gmail.com>
2015-05-25 10:10:52 -0700

    Add test for resume page

---
+++ spec/features/resume_spec.rb
@@ -0,0 +1,15 @@
+require "spec_helper"
+
+RSpec.describe "loading", :js => true, :type => :feature do
+  before do
+    visit "/#/resume"
+  end
+  
+  it "renders resume template" do
+    expect(page).to have_selector ".page-container .resume-page" 
+  end
+
+  it "embeds resume.html" do
+    expect(page).to have_text "LYLE FRANKLIN" 
+  end
+end
